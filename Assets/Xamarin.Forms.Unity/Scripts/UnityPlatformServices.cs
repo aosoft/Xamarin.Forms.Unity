c7daaaa9980a6b81b9414814e21fc55d47aab42e
aosoft <tan-y@aosoft.jp>
2017-08-11 19:30:09 +0900

    add mcs.rsp (-r options), *.meta, UnityPlatformServices.cs

---
+++ Assets/Xamarin.Forms.Unity/Scripts/UnityPlatformServices.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.Platform.Unity
+{
+	public class UnityPlatformServices : IPlatformServices
+	{
+		/*-----------------------------------------------------------------*/
+		#region Private Field
+
+		Thread _mainThread;
+
+		static readonly MD5CryptoServiceProvider _md5 = new MD5CryptoServiceProvider();
+
+		#endregion
+
+		/*-----------------------------------------------------------------*/
+		#region Constructor
+
+		public UnityPlatformServices(Thread mainThread)
+		{
+			_mainThread = mainThread;
+		}
+
+		#endregion
+
+		/*-----------------------------------------------------------------*/
+		#region IPlatformServices
+
+		public bool IsInvokeRequired => _mainThread.ManagedThreadId == Thread.CurrentThread.ManagedThreadId;
+
+		public string RuntimePlatform => "Unity";
+
+		public void BeginInvokeOnMainThread(Action action)
+		{
+			SynchronizationContext.Current.Post(_ => action(), null);
+		}
+
+		public Ticker CreateTicker()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Assembly[] GetAssemblies()
+		{
+			return null;// AppDomain.CurrentDomain.GetAssemblies();
+		}
+
+		public string GetMD5Hash(string input)
+		{
+			var bytes = _md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+			var sb = new StringBuilder();
+			foreach (var c in bytes)
+			{
+				sb.AppendFormat("{0:X2}", c);
+			}
+			return sb.ToString();
+		}
+
+		public double GetNamedSize(NamedSize size, Type targetElementType, bool useOldSizes)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Stream> GetStreamAsync(Uri uri, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IIsolatedStorageFile GetUserStoreForApplication()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void OpenUriAction(Uri uri)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void StartTimer(TimeSpan interval, Func<bool> callback)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
