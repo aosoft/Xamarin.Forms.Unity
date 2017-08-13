8ae8fd882aeb5824e958b55c886bc3aa72cbe0b7
aosoft <tan-y@aosoft.jp>
2017-08-14 07:15:32 +0900

    add UnityDeviceInfo, UnityPlatform

---
+++ Assets/Xamarin.Forms.Unity/Scripts/UnityDeviceInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.Platform.Unity
+{
+	internal class UnityDeviceInfo : DeviceInfo
+	{
+		Size _pixelScreenSize = new Size();
+		Size _scaledScreenSize = new Size();
+		double _scalingFactor = 1.0;
+
+		internal UnityDeviceInfo()
+		{
+			var bounds = new Size();
+			_pixelScreenSize = new Size(bounds.Width, bounds.Height);
+			_scaledScreenSize = _pixelScreenSize;
+		}
+
+		public override Size PixelScreenSize => _pixelScreenSize;
+
+		public override Size ScaledScreenSize => _scaledScreenSize;
+
+		public override double ScalingFactor => _scalingFactor;
+	}
+}
---
+++ Assets/Xamarin.Forms.Unity/Scripts/UnityPlatform.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Internals;
+
+
+namespace Xamarin.Forms.Platform.Unity
+{
+	public class UnityPlatform : IPlatform, IDisposable
+	{
+		/*-----------------------------------------------------------------*/
+		#region Private Field
+		#endregion
+
+		/*-----------------------------------------------------------------*/
+		#region Constructor / Dispose
+
+		public void Dispose()
+		{
+		}
+
+		#endregion
+
+		/*-----------------------------------------------------------------*/
+		#region Property
+
+
+
+		#endregion
+
+		/*-----------------------------------------------------------------*/
+		#region IPlatform
+
+		public SizeRequest GetNativeSize(VisualElement view, double widthConstraint, double heightConstraint)
+		{
+			throw new NotImplementedException();
+		}
+		
+		#endregion
+	}
+}
