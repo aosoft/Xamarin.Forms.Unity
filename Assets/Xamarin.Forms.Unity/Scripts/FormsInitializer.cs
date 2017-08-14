2b05e871194841a2342119f138afb256879f395a
aosoft <tan-y@aosoft.jp>
2017-08-14 22:40:32 +0900

    add FormsInitializer modified test scene (FormInitializer script attached to EventSystem, and Forms script removed)

---
+++ Assets/Xamarin.Forms.Unity/Scripts/FormsInitializer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.Platform.Unity
+{
+	/// <summary>
+	/// Xamarin.Forms の Forms 実装。
+	/// 通常の実装と異なり、どこかの UI EventSystem に AddComponent しておくことが Forms.Init 相当の処置になる。 
+	/// </summary>
+	public class FormsInitializer : MonoBehaviour
+	{
+		/*-----------------------------------------------------------------*/
+		#region Private Field
+
+		Thread _mainThread;
+
+		#endregion
+
+		/*-----------------------------------------------------------------*/
+		#region MonoBehavior
+
+		private void Awake()
+		{
+			/*
+			_mainThread = Thread.CurrentThread;
+
+			Device.PlatformServices = new UnityPlatformServices(this);
+			Device.SetIdiom(TargetIdiom.Desktop);
+			Device.Info = new UnityDeviceInfo();
+
+			Registrar.RegisterAll(new[]
+				{ typeof(ExportRendererAttribute), typeof(ExportCellAttribute), typeof(ExportImageSourceHandlerAttribute) });
+			ExpressionSearch.Default = new UnityExpressionSearch();
+			*/
+		}
+
+		private void OnDestroy()
+		{
+			/*
+			Device.PlatformServices = null;
+			Device.SetIdiom(TargetIdiom.Unsupported);
+			Device.Info = null;
+			*/
+		}
+
+		#endregion
+
+		/*-----------------------------------------------------------------*/
+		#region Property
+
+		public Thread MainThread => _mainThread;
+
+		#endregion
+	}
+}
