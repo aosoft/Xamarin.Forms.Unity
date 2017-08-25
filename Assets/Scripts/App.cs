c1626c15422977a1945b5312c2f22fd5c6962381
aosoft <tan-y@aosoft.jp>
2017-08-25 13:44:58 +0900

    implements Test App

--- Assets/Scripts/App.cs
+++ Assets/Scripts/App.cs
@@ -2,4 +2,29 @@
 
 public class App : Application
 {
+	public App()
+	{
+		var page = new ContentPage();
+		var label = new Label();
+		label.Text = "Hello, Xaramin.Forms!!";
+
+		page.Content = label;
+
+		MainPage = page;
+	}
+
+	protected override void OnStart()
+	{
+		base.OnStart();
+	}
+
+	protected override void OnSleep()
+	{
+		base.OnSleep();
+	}
+
+	protected override void OnResume()
+	{
+		base.OnResume();
+	}
 }
