4af7ca373896e439bf97f29dfc144f70bfcac6cd
aosoft <tan-y@aosoft.jp>
2017-09-07 08:01:54 +0900

    move xaml code to c# source (inline)

--- Assets/Scripts/Page1.xaml.cs
+++ Assets/Scripts/Page1.xaml.cs
@@ -16,9 +16,30 @@ public partial class Page1 : ContentPage
 
 	void InitializeComponent()
 	{
-		var path = System.IO.Path.Combine(UnityEngine.Application.streamingAssetsPath, "Page1.xaml");
-		var xaml = System.IO.File.ReadAllText(path);
-		Xamarin.Forms.Platform.Unity.XamlLoader.LoadXaml(this, xaml);
+		Xamarin.Forms.Platform.Unity.XamlLoader.LoadXaml(
+			this,
+			@"<?xml version=""1.0"" encoding=""utf-8"" ?>
+<ContentPage
+  xmlns=""http://xamarin.com/schemas/2014/forms""
+  xmlns:x=""http://schemas.microsoft.com/winfx/2009/xaml""
+  x:Class=""Page1"">
+  <Grid Padding=""16"">
+    <Grid.RowDefinitions>
+      <RowDefinition />
+      <RowDefinition Height=""32""/>
+    </Grid.RowDefinitions>
+
+    <Grid Grid.Row=""1"">
+      <Grid.ColumnDefinitions>
+        <ColumnDefinition />
+        <ColumnDefinition />
+      </Grid.ColumnDefinitions>
+      <Label Grid.Column=""0"" Text=""{Binding Counter.Value}"" HorizontalTextAlignment=""Center"" FontSize=""20""/>
+      <Button Grid.Column=""1"" Command=""{Binding InstantiateCommand}"" />
+    </Grid>
+  </Grid>
+</ContentPage>
+");
 	}
 }
 
