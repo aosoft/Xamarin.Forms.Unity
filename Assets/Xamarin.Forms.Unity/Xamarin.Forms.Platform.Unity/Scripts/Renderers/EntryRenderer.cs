40066347a5c3429856ea22e7f02cf883ac8a1fc8
aosoft <tan-y@aosoft.jp>
2017-09-11 13:48:12 +0900

    added EntryRenderer

---
+++ Assets/Xamarin.Forms.Unity/Xamarin.Forms.Platform.Unity/Scripts/Renderers/EntryRenderer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Xamarin.Forms.Platform.Unity
+{
+	public class EntryRenderer : ViewRenderer<Entry, UnityEngine.UI.InputField>
+	{
+		/*-----------------------------------------------------------------*/
+		#region Field
+
+
+		#endregion
+
+		/*-----------------------------------------------------------------*/
+		#region MonoBehavior
+
+
+		#endregion
+
+		/*-----------------------------------------------------------------*/
+		#region Event Handler
+
+		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged(e);
+
+			if (e.NewElement != null)
+			{
+
+			}
+		}
+
+		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+
+			base.OnElementPropertyChanged(sender, e);
+		}
+
+		#endregion
+
+		/*-----------------------------------------------------------------*/
+		#region Internals
+
+		#endregion
+	}
+}
