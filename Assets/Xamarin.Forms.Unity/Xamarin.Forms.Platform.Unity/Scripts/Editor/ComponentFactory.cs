55ee7ab882cdf46e3d335fbffc520e16c2eb8385
aosoft <tan-y@aosoft.jp>
2017-09-14 04:35:26 +0900

    added ComponentFactory (Activity Creator)

---
+++ Assets/Xamarin.Forms.Unity/Xamarin.Forms.Platform.Unity/Scripts/Editor/ComponentFactory.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Xamarin.Forms.Platform.Unity
+{
+
+	public static class ComponentFactory
+	{
+		[UnityEditor.MenuItem("GameObject/Xamarin.Forms Application Activity(User defined)", priority = 21)]
+		static void CreateFormsApplicationActivity()
+		{
+			var assem = typeof(UnityFormsApplicationActivity).Assembly;
+			foreach (var t in assem.GetTypes())
+			{
+				if (t.IsAbstract == false && t.IsGenericType == false &&
+					t.IsSubclassOf(typeof(UnityFormsApplicationActivity)))
+				{
+					var go = new GameObject();
+					go.name = string.Format("Xamarin.Forms Activity({0})", t.Name);
+					go.AddComponent(t);
+				}
+			}
+		}
+	}
+}
+
