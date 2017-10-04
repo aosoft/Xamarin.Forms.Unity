ee73a4d304f2bda4d6375d07b660ed96fb8b3d0c
aosoft <tan-y@aosoft.jp>
2017-10-04 20:53:27 +0900

    implements XamlPadBindingContext

--- Assets/Scripts/XamlPad/XamlPadBindingContext.cs
+++ Assets/Scripts/XamlPad/XamlPadBindingContext.cs
@@ -6,7 +6,6 @@ using System;
 
 namespace XamlPad
 {
-
 	public class XamlPadBindingContext
 	{
 		class InternalReactiveCommand : ReactiveCommand, ICommand
@@ -41,8 +40,47 @@ namespace XamlPad
 
 		public XamlPadBindingContext(MonoBehaviour disposer)
 		{
+			XamlSource = new InternalReactiveProperty<string>(disposer);
+			CompileResult = new InternalReactiveProperty<string>(disposer);
+			RootPage = new InternalReactiveProperty<RootPage>(disposer);
+
+			var cmd = new InternalReactiveCommand(disposer);
+			cmd.Subscribe(_ =>
+			{
+				try
+				{
+					var page = new RootPage();
+					Xamarin.Forms.Platform.Unity.XamlLoader.LoadXaml(page, XamlSource.Value);
+					RootPage.Value = page;
+				}
+				catch (Exception e)
+				{
+					CompileResult.Value = e.Message;
+				}
+
+			});
+			CompileCommand = cmd;
 		}
 
+		public ReactiveCommand CompileCommand
+		{
+			get;
+		}
+
+		public ReactiveProperty<string> XamlSource
+		{
+			get;
+		}
+
+		public ReactiveProperty<string> CompileResult
+		{
+			get;
+		}
+
+		public ReactiveProperty<RootPage> RootPage
+		{
+			get;
+		}
 	}
 
 }
