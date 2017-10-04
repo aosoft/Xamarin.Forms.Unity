fb3a4c407d44fd0e0a4f26a3c2a16a2fdfe2640e
aosoft <tan-y@aosoft.jp>
2017-10-04 13:44:03 +0900

    added XamlPad scene and scripts

---
+++ Assets/Scripts/XamlPad/App.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+
+namespace XamlPad
+{
+	public class App : Application
+	{
+		public App()
+		{
+		}
+
+		protected override void OnStart()
+		{
+			base.OnStart();
+		}
+
+		protected override void OnSleep()
+		{
+			base.OnSleep();
+		}
+
+		protected override void OnResume()
+		{
+			base.OnResume();
+		}
+	}
+}
---
+++ Assets/Scripts/XamlPad/XamlPadBindingContext.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UniRx;
+using System.Windows.Input;
+using System.ComponentModel;
+using System;
+
+namespace XamlPad
+{
+
+	public class XamlPadBindingContext
+	{
+		class InternalReactiveCommand : ReactiveCommand, ICommand
+		{
+			public InternalReactiveCommand(MonoBehaviour disposer)
+			{
+				CanExecute.Subscribe(_ => CanExecuteChanged?.Invoke(this, EventArgs.Empty)).AddTo(disposer);
+			}
+
+			public event EventHandler CanExecuteChanged;
+
+			void ICommand.Execute(object parameter)
+			{
+				Execute();
+			}
+
+			bool ICommand.CanExecute(object parameter)
+			{
+				return CanExecute.Value;
+			}
+		}
+
+		class InternalReactiveProperty<T> : ReactiveProperty<T>, INotifyPropertyChanged
+		{
+			public InternalReactiveProperty(MonoBehaviour disposer)
+			{
+				this.Subscribe(_ => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null))).AddTo(disposer);
+			}
+
+			public event PropertyChangedEventHandler PropertyChanged;
+		}
+
+		public XamlPadBindingContext(MonoBehaviour disposer)
+		{
+		}
+
+	}
+
+}
